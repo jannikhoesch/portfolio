06e3c9cebe25e5224768558bb7502bd8cec343a9
jannikhoesch <jannik.hoesch@outlook.de>
2025-03-12 21:02:58 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,27 +43,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-      <html
-          lang="en"
-          className={cx(
-              'text-black bg-white dark:text-white dark:bg-black',
-              GeistSans.variable,
-              GeistMono.variable
-          )}
-      >
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-      <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-        <Navbar />
-        {children}
-        <Footer />
-        <Analytics />
-        <SpeedInsights />
-      </main>
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
       </body>
-      </html>
-  );
+    </html>
+  )
 }
